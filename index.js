955ff985c9d5919a57f2600b60cef49ad499356d
clint <clint@clints-MacBook-Pro.local>
2024-10-18 09:18:36 +0800

    completed

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const productRoute = require('./routes/product.route.js')
+const Product = require("./models/product.model.js");
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+// routes
+app.use("/api/products", productRoute)
 
 
+app.get("/", (req, res) => {
+  res.send("Hello from Node API ser");
+});
 
-app.get('/', (req, res)=> {
-    res.send('Hello from Node API ser')
-})
 
-app.post('/api/products',(req, res)=>{
-    res.send('Data Received')
-})
+let localServer = 4000
 
-mongoose.connect("mongodb+srv://clint_tay:clinton2024@backend-simple.omh0a.mongodb.net/?retryWrites=true&w=majority&appName=backend-simple").then(()=> {
-    console.log('connected to the database');
-    app.listen(3000, ()=>{
-        console.log('server is running at 3000')
-    })
-})
-.catch(()=>{
-    console.log('connection failed')
-})
+mongoose
+  .connect(
+    "mongodb+srv://clint_tay:clinton2024@backend-simple.omh0a.mongodb.net/?retryWrites=true&w=majority&appName=backend-simple"
+  )
+  .then(() => {
+    console.log("connected to the database");
+    app.listen(localServer, () => {
+      console.log(`server is running at ${localServer}`);
+    });
+  })
+  .catch(() => {
+    console.log("connection failed");
+  });
